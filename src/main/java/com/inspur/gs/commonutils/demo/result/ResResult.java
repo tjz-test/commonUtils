269954c280ef9704d34474d594f76831d2e9d25f
tianjinzan01 <tianjinzan01@inspur.com>
2022-07-27 18:19:42 +0800

    update code

---
+++ src/main/java/com/inspur/gs/commonutils/demo/result/ResResult.java
@@ -0,0 +1,77 @@
+package com.inspur.gs.commonutils.demo.result;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author 77165
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ResResult<T> implements Serializable {  //该类为api,和web统一的返回格式标准化
+
+    private static final long serialVersionUID = 8676131899637805509L;
+
+    /**
+     * 1.status状态值：代表本次请求response的状态结果。
+     */
+    private Integer status;
+    /**
+     * 2.response描述：对本次状态码的描述。
+     */
+    private String message;
+    /**
+     * 3.data数据：本次返回的数据。
+     */
+    private T data;
+
+    /**
+     * 成功，创建ResResult：没data数据
+     */
+    public static <T> ResResult<T> success() {
+        ResResult<T> result = new ResResult<>();
+        result.setResultCode(ResultCode.SUCCESS);
+        return result;
+    }
+
+    /**
+     * 成功，创建ResResult：有data数据
+     */
+    public static <T> ResResult<T> success(T data) {
+        ResResult<T> result = new ResResult<>();
+        result.setResultCode(ResultCode.SUCCESS);
+        result.setData(data);
+        return result;
+    }
+
+    /**
+     * 失败，指定status、desc
+     */
+    public static <T> ResResult<T> fail(Integer status, String desc) {
+        ResResult<T> result = new ResResult<>();
+        result.setStatus(status);
+        result.setMessage(desc);
+        return result;
+    }
+
+    /**
+     * 失败，指定ResultCode枚举
+     */
+    public static <T> ResResult<T> fail(ResultCode resultCode) {
+        ResResult<T> result = new ResResult<>();
+        result.setResultCode(resultCode);
+        return result;
+    }
+
+    /**
+     * 把ResultCode枚举转换为ResResult
+     */
+    private void setResultCode(ResultCode code) {
+        this.status = code.code();
+        this.message = code.message();
+    }
+}
